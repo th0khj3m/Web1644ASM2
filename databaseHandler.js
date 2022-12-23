1018c2eaf7baa23752727500cb97749c8fd66fb4
Khj3m <109201777+lolvuongong@users.noreply.github.com>
2022-12-23 18:56:30 +0700

    Initial commit

---
+++ databaseHandler.js
@@ -0,0 +1,43 @@
+var MongoClient = require('mongodb').MongoClient
+var url = 'mongodb://0.0.0.0:27017'
+const { ObjectId } = require('bson')
+
+async function getDB() {
+    let client = await MongoClient.connect(url)
+    let db = client.db("ASM1644")
+    return db
+}
+
+async function insertNewProduct(newProduct) {
+    let db = await getDB()
+    let id = await db.collection("products").insertOne(newProduct)
+    return id
+}
+
+async function getAllProducts() {
+    let db = await getDB()
+    let results = await db.collection("products").find().toArray()
+    return results
+}
+
+async function deleteProductById(id) {
+    let db = await getDB()
+    await db.collection("products").deleteOne({_id: ObjectId(id)})
+}
+
+async function findProductById(id) {
+    let db = await getDB()
+    const productToEdit = await db.collection("products").findOne({_id: ObjectId(id)})
+    return productToEdit
+}
+
+async function updateProduct(id, name, price, picUrl) {
+    let db = await getDB()
+    await db.collection("products").updateOne({ _id: ObjectId(id) },
+        { $set: { "name": name, "price": price, "picture": picUrl } })
+}
+
+
+
+
+module.exports = {insertNewProduct, getAllProducts, deleteProductById, findProductById, updateProduct}
---
+++ main.js
@@ -0,0 +1,62 @@
+var express = require('express')
+var app = express()
+
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'));
+
+
+const cookieParser = require("cookie-parser")
+const session = require('express-session');
+const { insertNewProduct, getAllProducts, deleteProductById, updateProduct, findProductById } = require('./databaseHandler');
+
+//creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24
+
+app.get('/', async (req, res) => {
+    let results = await getAllProducts()
+    res.render('home', {results: results})
+})
+
+app.get('/insertProduct', (req, res) => {
+    res.render('insert')
+})
+
+app.post('/insertProduct', async (req, res) => {
+    const name = req.body.txtProductName
+    const price = req.body.txtProductPrice
+    const picUrl = req.body.txtProductPic
+    const newProduct = { 
+        name: name, 
+        price: Number.parseFloat(price),
+        picture: picUrl
+    }
+    await insertNewProduct(newProduct)
+    res.redirect('/')
+})
+
+app.get('/deleteProduct', async (req, res) => {
+    const id = req.query.id
+    await deleteProductById(id)
+    res.redirect('/')
+})
+
+app.get('/editProduct', async (req, res) => {
+    const id = req.query.id
+    const productToEdit = await findProductById(id)
+    res.render("edit", {product: productToEdit})
+})
+
+app.post('/editProduct', async (req, res) => {
+    const id = req.body.id
+    const name = req.body.txtProductName
+    const price = req.body.txtProductPrice
+    const picUrl = req.body.txtProductPic
+    await updateProduct(id, name, price, picUrl)
+    res.redirect('/')
+})  
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, (req, res) => {
+    console.log("Server is running at PORT: ", PORT)
+})
